6d90d1af14a57e98537fba4e3f89f6e36e142fbb
dawnik17 <dawnik17@gmail.com>
2023-08-16 19:39:16 +0530

    resnet encoder, custom decoder UNET

--- src/run/unet/inference.py
+++ src/run/unet/inference.py
@@ -1,10 +1,13 @@
 import os
 
 import albumentations as A
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import yaml
 from albumentations.pytorch import ToTensorV2
 from easydict import EasyDict
+from PIL import Image
 
 from src.models.unet.resunet import UNet as Model
 
@@ -63,3 +66,32 @@ class ResUnetInfer:
         output_tensor = torch.sigmoid(output_tensor)
 
         return output_tensor.permute(1, 2, 0).cpu().numpy()
+    
+    @staticmethod
+    def load_image_as_array(image_path):
+        # Load a PIL image
+        pil_image = Image.open(image_path)
+
+        # Convert PIL image to NumPy array
+        return np.array(pil_image.convert("RGB"))
+
+    @staticmethod
+    def plot_array(array: np.array, figsize=(10, 10)):
+        plt.figure(figsize=figsize)
+        plt.imshow(array)
+        plt.show()
+
+    @staticmethod
+    def save_numpy_as_image(numpy_array, image_path):
+        """
+        Saves a NumPy array as an image.
+        Args:
+            numpy_array (numpy.ndarray): The NumPy array to be saved as an image.
+            image_path (str): The path where the image will be saved.
+        """
+        # Convert the NumPy array to a PIL image
+        image = Image.fromarray(numpy_array)
+
+        # Save the PIL image to the specified path
+        image.save(image_path)
+
